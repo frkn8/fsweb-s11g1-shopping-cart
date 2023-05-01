637e102c8686fa8ff854fadb81ddab31e633c9ef
frkn8 <103068163+frkn8@users.noreply.github.com>
2023-05-01 23:37:52 +0300

    Tamamlandı..

--- src/App.js
+++ src/App.js
@@ -6,30 +6,67 @@ import { data } from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
+
+const writeToLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const readFromLocalStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+const LSKey = "g11s10223";
+
+const initializeCart = () => {
+  const cart = readFromLocalStorage(LSKey);
+  if (cart === null) {
+    return [];
+  } else {
+    return cart;
+  }
+};
 
 function App() {
   const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => initializeCart());
 
   const addItem = (item) => {
     // verilen itemi sepete ekleyin
+    const newCart = [...cart, item];
+
+    setCart(newCart);
+    writeToLocalStorage(LSKey, newCart);
+  };
+
+  const removeItem = (id) => {
+    const newRemove = [...cart];
+    let removedID = newRemove.findIndex((i) => i.id === id);
+    newRemove.splice(removedID, 1);
+    setCart(newRemove);
+    writeToLocalStorage(LSKey, newRemove);
   };
 
   return (
-    <div className="App">
-      <Navigation cart={cart} />
+    <ProductContext.Provider value={{ products, addItem }}>
+      <div className="App">
+        <CartContext.Provider value={{ cart, removeItem }}>
+          <Navigation />
 
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+          {/* Routelar */}
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
-    </div>
+            <Route path="/cart">
+              <ShoppingCart />
+            </Route>
+          </main>
+        </CartContext.Provider>
+      </div>
+    </ProductContext.Provider>
   );
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,28 +1,28 @@
-import React from "react";
 import styled from "styled-components";
+import { useContext } from "react";
 
 // Components
 import Product from "./Product";
+import { ProductContext } from "../contexts/ProductContext";
 
 const ScProducts = styled.section`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1.5rem;
-
   @media (max-width: 480px) {
     grid-template-columns: repeat(1, 1fr);
-
     img {
       width: 100%;
     }
   }
 `;
 
-const Products = (props) => {
+const Products = () => {
+  const { products, addItem } = useContext(ProductContext);
   return (
     <ScProducts>
-      {props.products.map((product) => (
-        <Product key={product.id} product={product} addItem={props.addItem} />
+      {products.map((product) => (
+        <Product key={product.id} product={product} addItem={addItem} />
       ))}
     </ScProducts>
   );
